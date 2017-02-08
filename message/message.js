e8ee21d2df0fa3a45c5ef63afe4264e1a7a0000d
Pablo Fonseca <pablofonseca777@gmail.com>
2017-02-08 01:29:51 -0200

    Add mustache and tests

--- message/message.js
+++ message/message.js
@@ -1,5 +1,8 @@
 var readTextFile = require('./readTextFile');
+var mustacheTemplate = require('./mustacheTemplate');
+var marked = require('marked');
 
 module.exports = {
   readTextFile: readTextFile
+  marked: marked
 };
--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 var roast = require('roast.it')
 
 require('./blogRouteTest');
+require('./mustacheTemplateTest');
 
 roast.run();
 roast.exit();
