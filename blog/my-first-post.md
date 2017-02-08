f71ec4a48d0bcbfa259304c4ef58dba75f2dadbd
Pablo Fonseca <pablofonseca777@gmail.com>
2017-02-08 01:44:28 -0200

    Small Changes and Finishing up

---
+++ blog/my-first-post.md
@@ -0,0 +1,3 @@
+# My First Post
+
+Amet repellendus eveniet sit at tempore, ipsa! Dolorum ipsam reiciendis impedit corporis atque Officiis iusto placeat quasi vero atque, nostrum animi et Dicta qui placeat minus eius ab, nostrum placeat.
--- message/message.js
+++ message/message.js
@@ -1,8 +1,9 @@
-var readTextFile = require('./readTextFile');
 var mustacheTemplate = require('./mustacheTemplate');
+var readTextFile = require('./readTextFile');
 var marked = require('marked');
 
 module.exports = {
+  mustacheTemplate: mustacheTemplate,
   readTextFile: readTextFile,
   marked: marked
 };
